b72028f99a33524320190e7fb0a656b0239714c7
Ben Brewer <benbrewerbowman@gmail.com>
2018-01-04 14:40:35 -0700

    Store methods

---
+++ CreateStoreExample.js
@@ -0,0 +1,15 @@
+import { createStore } from 'redux';
+
+
+const store = createStore(counter);
+
+const render = () => {
+  document.body.innerText = store.getState();
+}
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+  store.dispatch({ type: 'INCREMENT' });
+});
